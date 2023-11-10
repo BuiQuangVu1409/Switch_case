fb742ba8652c703ec4ea1a7a6d74beb762bb151b
BuiQuangVu1409 <buiquangvu149@gmail.com>
2023-11-10 16:30:31 +0700

    switch_case

---
+++ switch_case.js
@@ -0,0 +1,13 @@
+let browser = prompt("Enter browser name!")
+switch (browser) {
+    case 'Edge' :
+        alert("You've got the Edge!");
+        break;
+    case 'Chrom' :
+    case 'Firefox' :
+    case 'Opera' :
+        alert('Okay we support these browsers too');
+        break;
+    default :
+        alert('We hope that this page looks ok!');
+}
